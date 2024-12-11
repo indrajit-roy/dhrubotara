8c9c0befec2a38ad14051fadc602fe672cf21bc7
indrajit-roy-sc <indrajit.roy@smallcase.com>
2024-12-11 19:49:47 +0530

    refactor: rename products

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,7 +3,7 @@ import { Product } from "@/types";
 export const products: Product[] = [
   {
     id: '1',
-    name: 'Neem Face Wash',
+    name: 'Coconut Oil',
     description: 'Natural face wash with neem extract',
     price: 12.99,
     image: 'https://vanitywagon.in/cdn/shop/products/1_67883fa2-4afe-4fe1-bbb6-e823ca8b0681_1080x1080.jpg?v=1638343835',
@@ -11,7 +11,7 @@ export const products: Product[] = [
   },
   {
     id: '2',
-    name: 'Tulsi Hair Oil', 
+    name: 'Cough Relief', 
     description: 'Traditional hair oil with tulsi',
     price: 15.99,
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtqFOYVKyIAn1lbIJHWpHCOJN0hetRnoxHZQ&s',
@@ -19,18 +19,10 @@ export const products: Product[] = [
   },
   {
     id: '3',
-    name: 'Haldi Face Mask',
+    name: 'Digestive Aid',
     description: 'Brightening face mask with turmeric',
     price: 9.99,
     image: 'https://vanitywagon.in/cdn/shop/products/1_67883fa2-4afe-4fe1-bbb6-e823ca8b0681_1080x1080.jpg?v=1638343835',
     category: 'face'
-  },
-  {
-    id: '4',
-    name: 'Amla Hair Mask',
-    description: 'Nourishing hair mask with amla',
-    price: 14.99,
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtqFOYVKyIAn1lbIJHWpHCOJN0hetRnoxHZQ&s',
-    category: 'hair'
   }
 ];
