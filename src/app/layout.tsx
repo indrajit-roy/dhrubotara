19297d8170383b2bd0c24557afe9d2ef035406c2
indrajit-roy-sc <indrajit.roy@smallcase.com>
2024-12-12 18:28:05 +0530

    perf: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Providers from "@/components/Providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
